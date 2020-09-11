4c0785361c0358f359eb73e294dd296ab62b91c0
igor matos ribeiro <igortecnologia2020@gmail.com>
2020-09-11 12:10:23 -0300

    Feito atividade letra de musica aula pc

---
+++ Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace Musica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+             //<---Começo(Titulo)--->//
+            Console.Write("Compositores ");
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.Write("Herbert Lemos De Sou Vianna");
+            Console.Write("\n");
+            Console.ResetColor();
+
+            Console.Write("Cantor: ");
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine("Os paralamas do sucesso/Musica: Me liga");
+            Console.Write("\n");
+            Console.ResetColor();
+            //<---Final(Titulo)--->//
+
+            Console.WriteLine("Eu sei, jogos de amor são pra se jogar");
+            Console.WriteLine("Ah, por favor, não vem me explicar");
+            Console.WriteLine("O nosso jogo não tem regras nem juiz");
+            Console.WriteLine("Você não sabe quantos planos eu já fiz");
+            Console.Write("\n");
+
+            Console.WriteLine("Tudo que eu tinha pra perder eu já perdi");
+            Console.WriteLine("O seu exército invadindo o meu país");
+            Console.WriteLine("Se você lembrar, se quiser jogar");
+            Console.WriteLine("Me liga, me liga");
+            Console.Write("\n");
+
+            //<---Começo(Parte I)--->//
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("Mas sei, que não se pode terminar assim");
+            Console.WriteLine("O jogo segue e nunca chega ao fim");
+            Console.WriteLine("E recomeça a cada instante a cada instante");
+            Console.WriteLine("Eu não te peço muita coisa só uma chance");
+            Console.Write("\n");
+
+            Console.WriteLine("Pus no meu quarto, seu retrato na estante");
+            Console.WriteLine("Quem sabe um dia eu vou te ter ao meu alcance");
+            Console.WriteLine("Ai como ia ser bom se você deixasse");
+            Console.WriteLine("Se você lembrar, se quiser jogar");
+            Console.Write("\n");
+
+            Console.WriteLine("Me liga");
+            Console.WriteLine("Me liga");
+            Console.ResetColor();
+            Console.Write("\n");
+            //<---Final(Parte II)--->//
+
+            Console.WriteLine("Eu não te peço muita coisa só uma chance");
+            Console.WriteLine("Pus no meu quarto seu retrato na estante");
+            Console.WriteLine("Quem sabe um dia eu vou te ter ao meu alcance");
+            Console.WriteLine("Ai como ia ser bom se você deixasse");
+            Console.Write("\n");
+
+            Console.WriteLine("Se quiser lembrar, se quiser jogar");
+            Console.WriteLine("Me liga");
+            Console.WriteLine("Me liga");
+            Console.WriteLine("ahhhhh.ahhhh.ahhh.ahh.ah...");
+            Console.Write("\n");
+            Console.ResetColor();
+
+            //<---(fim)-->//
+        }
+    }
+}
